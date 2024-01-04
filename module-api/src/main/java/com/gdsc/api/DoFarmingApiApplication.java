cda3c27e36e9e1462e2a7e20c4e41dbcd3ae8460
Cho, Yonghyun <chooh1010@gamil.com>
2024-01-05 08:44:21 +0900

    chore: initial project

---
+++ module-api/src/main/java/com/gdsc/api/DoFarmingApiApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoFarmingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoFarmingApiApplication.class, args);
+    }
+
+}
