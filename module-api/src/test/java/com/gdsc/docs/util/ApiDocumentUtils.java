d0defb3ea16b930b93d6e8e62c211268c52b2a71
Cho, Yonghyun <chooh1010@gamil.com>
2024-01-29 21:31:41 +0900

    docs: swagger and rest docs setting

---
+++ module-api/src/test/java/com/gdsc/docs/util/ApiDocumentUtils.java
@@ -0,0 +1,30 @@
+package com.gdsc.docs.util;
+
+import org.springframework.restdocs.headers.RequestHeadersSnippet;
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+public interface ApiDocumentUtils {
+    static OperationRequestPreprocessor getDocumentRequest() {
+        return preprocessRequest(
+                modifyUris()
+                        .scheme("https")
+                        .host("dofarming.duckdns.org")
+                        .removePort()
+                , prettyPrint());
+    }
+
+    static OperationResponsePreprocessor getDocumentResponse() {
+        return preprocessResponse(prettyPrint());
+    }
+
+    static RequestHeadersSnippet getAuthorizationHeader(){
+        return requestHeaders(
+                headerWithName("Authorization").description("Bearer Token")
+        );
+    }
+}
