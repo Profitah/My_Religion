de2625f60f005f8aadbe5bab56f30b548e4d35fb
Cho, Yonghyun <chooh1010@gamil.com>
2024-02-07 15:19:50 +0900

    feat: add exceptions

--- module-core/src/main/java/com/gdsc/common/exception/ApplicationErrorType.java
+++ module-core/src/main/java/com/gdsc/common/exception/ApplicationErrorType.java
@@ -15,6 +15,9 @@ public enum ApplicationErrorType {
     NO_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
     //404
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    TRACK_NOT_FOUND(HttpStatus.NOT_FOUND, "트랙을 찾을 수 없습니다."),
+    ROUTINE_NOT_FOUND(HttpStatus.NOT_FOUND, "루틴을 찾을 수 없습니다."),
+    KEYWORD_NOT_FOUND(HttpStatus.NOT_FOUND, "키워드를 찾을 수 없습니다."),
     //500
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 발생했습니다. 잠시만 기달려주세요.");
 
