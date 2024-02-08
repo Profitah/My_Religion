c328cab727db5551ed4f101b83fdeac1c28e7dc5
Cho, Yonghyun <chooh1010@gamil.com>
2024-02-08 23:28:59 +0900

    fix: user info dto

--- module-api/src/main/java/com/gdsc/domain/user/model/UserInfoRequest.java
+++ module-api/src/main/java/com/gdsc/domain/user/model/UserInfoRequest.java
@@ -4,7 +4,7 @@ package com.gdsc.domain.user.model;
 import com.gdsc.domain.user.entity.Gender;
 import jakarta.validation.constraints.NotBlank;
 
-public record UserRequest(
+public record UserInfoRequest(
         @NotBlank
         String nickname,
         @NotBlank
--- module-api/src/main/java/com/gdsc/domain/user/model/UserInfoResponse.java
+++ module-api/src/main/java/com/gdsc/domain/user/model/UserInfoResponse.java
@@ -5,12 +5,12 @@ import com.gdsc.domain.user.entity.User;
 import lombok.Builder;
 
 @Builder
-public record UserResponse(
+public record UserInfoResponse(
         String nickname,
         Gender gender,
         Integer age) {
-    public static UserResponse of(User user) {
-        return UserResponse.builder()
+    public static UserInfoResponse of(User user) {
+        return UserInfoResponse.builder()
                 .nickname(user.getNickname())
                 .gender(user.getGender())
                 .age(user.getAge())
