c0ffd6d11a8b6d080ab428474fa6d73c65934d3e
Cho, Yonghyun <chooh1010@gamil.com>
2024-02-08 23:32:46 +0900

    docs: user mood api

--- module-api/src/test/java/com/gdsc/domain/user/controller/UserControllerTest.java
+++ module-api/src/test/java/com/gdsc/domain/user/controller/UserControllerTest.java
@@ -3,7 +3,8 @@ package com.gdsc.domain.user.controller;
 import com.gdsc.domain.user.entity.User;
 import com.gdsc.common.security.WithAuthUser;
 import com.gdsc.docs.util.RestDocsTest;
-import com.gdsc.domain.user.model.UserRequest;
+import com.gdsc.domain.user.model.UserInfoRequest;
+import com.gdsc.domain.user.model.UserMoodRequest;
 import com.gdsc.domain.user.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,7 +24,6 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("UserController 테스트")
@@ -40,7 +40,7 @@ class UserControllerTest extends RestDocsTest {
     @WithAuthUser
     void update_user_info() throws Exception {
         //given
-        given(userService.updateUserInfo(any(UserRequest.class), any(User.class))).willReturn(유저1);
+        given(userService.updateUserInfo(any(UserInfoRequest.class), any(User.class))).willReturn(유저1);
         //when & then
         mvc.perform(patch(REQUEST_URL + "/info")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -51,8 +51,36 @@ class UserControllerTest extends RestDocsTest {
                         getDocumentRequest(),
                         getDocumentResponse(),
                         getAuthorizationHeader(),
-                        requestFieldsByUser(),
-                        responseFieldsByUser()));
+                        requestFields(
+                                fieldWithPath("nickname").type(JsonFieldType.STRING).description("변경할 닉네임"),
+                                fieldWithPath("gender").type(JsonFieldType.STRING).description("변경할 성별"),
+                                fieldWithPath("age").type(JsonFieldType.NUMBER).description("변경할 나이")
+                        ),
+                        responseFieldsByUserInfo()));
+    }
+
+    @Test
+    @DisplayName("사용자의 기분을 변경한다.")
+    @WithAuthUser
+    void update_user_mood() throws Exception {
+        //given
+        given(userService.updateUserMood(any(UserMoodRequest.class), any(User.class))).willReturn(유저1);
+        //when & then
+        mvc.perform(patch(REQUEST_URL + "/mood")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(createJson(유저_기분_변경_요청1))
+                        .header("Authorization", "Bearer FirebaseToken"))
+                .andExpect(status().isOk())
+                .andDo(document("user-mood-update",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        getAuthorizationHeader(),
+                        requestFields(
+                                fieldWithPath("mood").type(JsonFieldType.STRING).description("변경할 기분")
+                        ),
+                        responseFields(
+                                fieldWithPath("mood").type(JsonFieldType.STRING).description("기분")
+                        )));
     }
 
     @Test
@@ -70,11 +98,11 @@ class UserControllerTest extends RestDocsTest {
                         responseFieldsByUser()));
     }
 
-    private static RequestFieldsSnippet requestFieldsByUser(){
-        return requestFields(
-                fieldWithPath("nickname").type(JsonFieldType.STRING).description("변경할 닉네임"),
-                fieldWithPath("gender").type(JsonFieldType.STRING).description("변경할 성별"),
-                fieldWithPath("age").type(JsonFieldType.NUMBER).description("변경할 나이")
+    private static ResponseFieldsSnippet responseFieldsByUserInfo() {
+        return responseFields(
+                fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임"),
+                fieldWithPath("gender").type(JsonFieldType.STRING).description("성별"),
+                fieldWithPath("age").type(JsonFieldType.NUMBER).description("나이")
         );
     }
 
@@ -82,7 +110,8 @@ class UserControllerTest extends RestDocsTest {
         return responseFields(
                 fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임"),
                 fieldWithPath("gender").type(JsonFieldType.STRING).description("성별"),
-                fieldWithPath("age").type(JsonFieldType.NUMBER).description("나이")
+                fieldWithPath("age").type(JsonFieldType.NUMBER).description("나이"),
+                fieldWithPath("mood").type(JsonFieldType.STRING).description("기분")
         );
     }
 
--- module-api/src/test/java/com/gdsc/fixture/DomainFixture.java
+++ module-api/src/test/java/com/gdsc/fixture/DomainFixture.java
@@ -7,9 +7,11 @@ import com.gdsc.domain.routine.model.RoutineRequest;
 import com.gdsc.domain.track.entity.Track;
 import com.gdsc.domain.track.model.TrackRequest;
 import com.gdsc.domain.user.entity.Gender;
+import com.gdsc.domain.user.entity.Mood;
 import com.gdsc.domain.user.entity.Role;
 import com.gdsc.domain.user.entity.User;
-import com.gdsc.domain.user.model.UserRequest;
+import com.gdsc.domain.user.model.UserInfoRequest;
+import com.gdsc.domain.user.model.UserMoodRequest;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,6 +24,7 @@ public class DomainFixture {
             .email("dofarming@gmail.com")
             .gender(Gender.MALE)
             .age(0)
+            .mood(Mood.CALM)
             .role(Role.ROLE_USER)
             .build();
 
@@ -31,6 +34,7 @@ public class DomainFixture {
             .email("dofarming@gmail.com")
             .gender(Gender.MALE)
             .age(0)
+            .mood(Mood.CALM)
             .role(Role.ROLE_USER)
             .build();
 
@@ -128,7 +132,8 @@ public class DomainFixture {
 
     public static final List<Routine> 루틴_초기_리스트 = Arrays.asList(루틴4, 루틴5, 루틴6);
 
-    public static final UserRequest 유저_정보_변경_요청1 = new UserRequest("닉네임 변경", Gender.MALE, 20);
+    public static final UserInfoRequest 유저_정보_변경_요청1 = new UserInfoRequest("닉네임 변경", Gender.MALE, 20);
+    public static final UserMoodRequest 유저_기분_변경_요청1 = new UserMoodRequest(Mood.HAPPY);
 
     public static final KeywordRequest 유저_키워드_저장_요청1 = new KeywordRequest(List.of("키워드1", "키워드2", "키워드3"));
 
