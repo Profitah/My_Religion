5ee612374c9cb01fed857d4ce36b20154cca9ffe
Cho, Yonghyun <chooh1010@gamil.com>
2024-02-13 00:14:51 +0900

    feat: add track date, status api

--- module-api/src/main/java/com/gdsc/domain/track/model/TrackRequest.java
+++ module-api/src/main/java/com/gdsc/domain/track/model/TrackRequest.java
@@ -1,11 +1,19 @@
 package com.gdsc.domain.track.model;
 
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public record TrackRequest(
         @NotBlank
-        String content
+        String content,
+        @NotBlank
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        LocalDate startDate,
+        @NotBlank
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        LocalDate endDate
 ) {
 }
--- module-api/src/main/java/com/gdsc/domain/track/model/TrackResponse.java
+++ module-api/src/main/java/com/gdsc/domain/track/model/TrackResponse.java
@@ -1,17 +1,29 @@
 package com.gdsc.domain.track.model;
 
 import com.gdsc.domain.track.entity.Track;
+import com.gdsc.domain.track.entity.TrackStatus;
 import lombok.Builder;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Builder
 public record TrackResponse(
         Long trackId,
-        String content
+        String content,
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        LocalDate startDate,
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        LocalDate endDate,
+        TrackStatus trackStatus
 ) {
     public static TrackResponse of(Track track) {
         return TrackResponse.builder()
                 .trackId(track.getId())
                 .content(track.getContent())
+                .startDate(track.getStartDate())
+                .endDate(track.getEndDate())
+                .trackStatus(track.getTrackStatus())
                 .build();
     }
 }
